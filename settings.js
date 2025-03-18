75ffa6f5694515c1c8f72b48cbe791aec4df72b6
Faiede <130595485+Faiede@users.noreply.github.com>
2025-03-18 08:36:16 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'nigger',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "nigger Bot",
+  botOwner: 'Ryan Gosling', // Your name
+  ownerNumber: '18687670763', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
